faed3543af8bc3ce40f670ef0f2f4525cc524516
kenjiuno <h8mastre@gmail.com>
2020-02-10 22:41:41 +0900

    Using wx flavor.

--- pcsx2/Haxkh2fm.cpp
+++ pcsx2/Haxkh2fm.cpp
@@ -4,6 +4,8 @@
 #include "Memory.h"
 #include "Hw.h"
 #include "R5900.h"
+#include "Utilities/Path.h"
+#include "gui/AppConfig.h"
 
 #include "Haxkh2fm.h"
 #include <stdio.h>
@@ -24,33 +26,22 @@ u32 g_injectSize = 0; // 0=expose-data, not 0=do-not-expose-data
 
 u32 inject_S_IEXPA()
 {
-    char fin[128 + 1];
-    FILE *f;
-    int i;
-    _snprintf(fin, 128, "inject.%08x\\%s", ElfCRC, s_fn);
-    fin[128] = 0;
-    for (i = 0; fin[i] != 0; i++) {
-        if (fin[i] == '/')
-            fin[i] = '\\';
-    }
+    wxFileName injectFrom =
+        PathDefs::GetProgramDataDir() + wxFileName(wxString::Format(L"inject.%08x\\%s", ElfCRC, WX_STR(wxString(s_fn))), wxPATH_NATIVE);
 
-    f = fopen(fin, "rb");
-    if (f != NULL) {
-        fseek(f, 0, SEEK_END);
-        long fileSize = ftell(f);
-        rewind(f);
-        size_t cnt = fread(PSM(s_addrData), 1, fileSize, f);
+    if (injectFrom.FileExists()) {
+        wxFile file(injectFrom.GetFullPath());
 
-        if (cnt == fileSize) {
-            Console.WriteLn(Color_StrongCyan, "#     S_IEXPA: Inject ok. %u ", 0U + cnt);
-            fclose(f);
+        wxFileOffset fileSize = file.Length();
 
-            return cnt;
+        ssize_t readSize = file.Read(PSM(s_addrData), fileSize);
+
+        if (readSize == fileSize) {
+            Console.WriteLn(Color_StrongCyan, "#     S_IEXPA: Inject ok. %u ", 0U + readSize);
+            return readSize;
         } else {
-            Console.WriteLn(Color_StrongCyan, "#     S_IEXPA: Inject failed ", 0U + cnt);
+            Console.WriteLn(Color_StrongCyan, "#     S_IEXPA: Inject failed ", 0U + readSize);
         }
-
-        fclose(f);
     }
 
     return 0;
@@ -59,8 +50,9 @@ u32 inject_S_IEXPA()
 u32 Haxkh2fm_trap_S_IEXPA_rec()
 {
     s_fn[32] = 0;
-    for (int x = 0; x < 32; x++)
+    for (int x = 0; x < 32; x++) {
         s_fn[x] = memRead8(cpuRegs.GPR.n.a0.UL[0] + x);
+    }
 
     s_addrData = cpuRegs.GPR.n.a1.UL[0];
 
@@ -72,8 +64,9 @@ u32 Haxkh2fm_trap_S_IEXPA_rec()
 u32 Haxkh2fm_trap_S_IEXPA_int()
 {
     s_fn[32] = 0;
-    for (int x = 0; x < 32; x++)
+    for (int x = 0; x < 32; x++) {
         s_fn[x] = memRead8(cpuRegs.GPR.n.a0.UL[0] + x);
+    }
 
     s_addrData = cpuRegs.GPR.n.a1.UL[0];
 
@@ -82,47 +75,24 @@ u32 Haxkh2fm_trap_S_IEXPA_int()
     return g_injectSize = inject_S_IEXPA();
 }
 
-static void prepdir(char *fout)
-{
-    char *p = fout;
-    while (true) {
-        p = strchr(p, '\\');
-        if (p == NULL)
-            break;
-        *p = 0;
-        _mkdir(fout);
-        *p = '\\';
-        p++;
-    }
-}
-
 void Haxkh2fm_trap_E_IEXPA()
 {
     if (s_fn[0] != 0 && s_addrData != 0 && g_injectSize == 0) {
         const u32 sizeDest = cpuRegs.GPR.n.v0.UL[0];
         {
             // extraction
+            wxFileName extractTo =
+                PathDefs::GetProgramDataDir() + wxFileName(wxString::Format(L"expa.%08x\\%s", ElfCRC, WX_STR(wxString(s_fn))), wxPATH_NATIVE);
 
-            char fout[128 + 1];
-            FILE *f;
-            int i;
-            _snprintf(fout, 128, "expa.%08x\\%s", ElfCRC, s_fn);
-            fout[128] = 0;
-            for (i = 0; fout[i] != 0; i++) {
-                if (fout[i] == '/')
-                    fout[i] = '\\';
-            }
+            if (extractTo.Mkdir(511, wxPATH_MKDIR_FULL)) {
+                wxFile file(extractTo.GetFullPath(), wxFile::write);
 
-            prepdir(fout);
+                if (file.IsOpened()) {
+                    file.Write(PSM(s_addrData), sizeDest);
+                    file.Close();
 
-            f = fopen(fout, "wb");
-            if (f != NULL) {
-                //Console::Notice("# cb=%u ", params sizeDest);
-
-                fwrite(PSM(s_addrData), sizeDest, 1, f);
-                fclose(f);
-
-                Console.WriteLn(Color_StrongCyan, "#     E_IEXPA: Out ok. ");
+                    Console.WriteLn(Color_StrongCyan, "#     E_IEXPA: Out ok. ");
+                }
             }
         }
     }
@@ -134,42 +104,33 @@ void Haxkh2fm_trap_E_IEXPA()
 void Haxkh2fm_trap_S_FINDX_int()
 {
     s_findfn[32] = 0;
-    for (int x = 0; x < 32; x++)
+    for (int x = 0; x < 32; x++) {
         s_findfn[x] = memRead8(cpuRegs.GPR.n.a1.UL[0] + x);
+    }
 }
 
 void Haxkh2fm_trap_S_FINDX_rec()
 {
     s_findfn[32] = 0;
-    for (int x = 0; x < 32; x++)
+    for (int x = 0; x < 32; x++) {
         s_findfn[x] = memRead8(cpuRegs.GPR.n.a1.UL[0] + x);
+    }
 }
 
 void Haxkh2fm_trap_E_FINDX()
 {
-    {
-        // test
+    wxFileName injectFrom =
+        PathDefs::GetProgramDataDir() + wxFileName(wxString::Format(L"inject.%08x\\%s", ElfCRC, WX_STR(wxString(s_findfn))), wxPATH_NATIVE);
 
-        char fin[128 + 1];
-        int i;
-        _snprintf(fin, 128, "inject.%08x\\%s", ElfCRC, s_findfn);
-        fin[128] = 0;
-        for (i = 0; fin[i] != 0; i++) {
-            if (fin[i] == '/')
-                fin[i] = '\\';
-        }
+    if (injectFrom.FileExists()) {
+        u32 addr = cpuRegs.GPR.n.v0.UL[0];
+        if (addr != 0) {
+            u32 sizeFrm = memRead32(addr + 12);
+            u32 sizeNew = injectFrom.GetSize().ToULong();
 
-        struct stat st;
-        if (0 == stat(fin, &st)) {
-            u32 addr = cpuRegs.GPR.n.v0.UL[0];
-            if (addr != 0) {
-                u32 sizeFrm = memRead32(addr + 12);
-                u32 sizeNew = st.st_size;
-
-                if (sizeFrm != sizeNew) {
-                    memWrite32(addr + 12, sizeNew);
-                    Console.WriteLn(Color_StrongCyan, "#     E_FINDX: Entry %s resized to %u (from %u) ", s_findfn, sizeNew, sizeFrm);
-                }
+            if (sizeFrm != sizeNew) {
+                memWrite32(addr + 12, sizeNew);
+                Console.WriteLn(Color_StrongCyan, "#     E_FINDX: Entry %s resized to %u (from %u) ", s_findfn, sizeNew, sizeFrm);
             }
         }
     }
